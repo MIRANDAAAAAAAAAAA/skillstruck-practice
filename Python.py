435e6881a729b67f643a48f3a8f85c752f10cdcf
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-13 13:20:11 -0700

    exported from Skill Struck

---
+++ Python.py
@@ -0,0 +1,181 @@
+string1= "My mom went to buy food "
+string2= "at Target"
+string3= string1 + string2
+
+print(string3)
+
+flowers = 200
+trees = 4
+
+answer = flowers / trees
+print(answer)
+
+number_flowers =50
+
+print(number_flowers + 15)
+
+nubmer_SVT = 13
+number_infinity = 8
+
+total_infinity = nubmer_SVT * number_infinity
+print(total_infinity)
+
+Flowers = 8 % 13
+print(Flowers)
+
+name = "Miranda"
+age = 16
+happy = True
+
+caterpillars = 3
+
+leaves = 25
+
+caterpillars = caterpillars - 2
+print(caterpillars)
+
+rocks = 15
+
+print(rocks / 3)
+
+print(5 + 3 * 4 + 3)
+
+c = 20 * 5
+print(c)
+
+apples = 10
+oranges = 5
+print(apples + oranges)
+
+z = 15 / 3
+print(z)
+
+a = 7
+b = 3
+print(a * b)
+
+c = 18 / 2
+print(c)  
+
+c = a / b
+print(c)
+
+c = a / b
+print(c)
+
+string1 = '''I am part of the gardening club.
+This class takes place in the garden outside the class.
+and I still hvae time to do my homework
+also the teacher is supper nice
+we are assinged to the same teacher.'''
+print(string1)
+
+string1 = "I love Seventeen"
+print(string1.lower())
+print(string1.upper())
+
+string1 = "Seventeen is so cool of course"
+print(string1.replace("cool", "especily THE8"))
+string1 = "Seventeen is so cool especily THE8"
+print(string1.upper())
+
+string = "I love dinosaurs so much!"
+
+print(string.replace("I", ""))
+
+string = "I love otters so much!"
+
+print(string.replace("otters", "manatees"))
+
+string1 = "My name is Simon." 
+print(string1.lower())
+
+string1 = "My name is Simon." 
+print(string1.upper())
+
+string1 = "Let's go swimming." 
+print(string1.replace("swimming", "hiking"))
+
+string1 = "Let's go swimming." 
+print(string1.replace("swimming", "hiking"))
+string1 = " I love Seventeen so much more Xuminghao or more known as THE8! "
+print(string1.strip())
+
+string1 = " I LOVE MINGHAO THE MOST OUT OF EVERY MEMBER!"
+Check ="love" in string1
+print(Check)
+
+#output"FALSE"
+
+string1 = " I love my mom!"
+check = "love myself" not in string1
+print(check) 
+
+#outputs " False"
+
+string1 = "the number of members in seventeen are {}"
+SVT = 13
+print(string1.format(SVT)) 
+
+string1 = "There are about {} patatos in my hands"
+SVT = 7473587344355
+print(string1.format(SVT))
+
+string1 = " Motiaa ate about {} cookies yesterday"
+SVT = 887583399545437600132435475775
+print(string1.format(SVT))
+
+SVT = 8875833
+
+string1 = "Motiaa ate about {} frogs and then she ate {} some pennies for lunch."
+SVT = 12
+TXT = 35
+print(string1.format(SVT,TXT))
+
+string1 = ("I got {} cookies at the bakery")
+total = 13 
+print(string1.format(total))
+classroom = "I have {} kids in my class." 
+students = 25 
+print(classroom.format(students))
+
+
+string1 = " The number of members is {}. I thought because the groups name was SEVENTEEN it was {} but it is not."
+SVT = 13
+TXT = 17
+print(string1.format(SVT,TXT))
+
+string1 = "The number of members is {0}. I thought because the groups name was SEVENTEEN it was {1} but it is not."
+SVT = 21
+TXT = 9817289
+print(string1.format(SVT,TXT))
+
+string1 = "The total number of miles we hiked is {1}. We saw {0} deer"
+miles = 21
+animals = 20
+print(string1.format(animals, miles))
+
+
+distance = [5, 7, 20, 11, 18, 90]
+print(distance)
+
+pets = ["dog", "cat", 3, 5, "chicken"] 
+print(pets)
+
+SVT_pet = "frog"
+your_pet = "poodle"
+
+if SVT_pet == your_pet:
+    print("You and your firend have the same pet!")
+else:
+    print("You and your firend have different petss.")
+
+SEVENTEEN_ages = {27,28,25,26,29}
+
+for x in SEVENTEEN_ages:
+    if x >= 24: 
+        print("Minghao is the third to youngest in the group")
+    else:
+        print("Minghao is not the third to younget in the g
+
+colo
