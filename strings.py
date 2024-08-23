89baa64f5d96ac58381753fb83348bf33fedefba
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-23 12:20:03 -0700

    exported from Skill Struck

---
+++ strings.py
@@ -0,0 +1,53 @@
+# # string1 = """ Hello I am Miranda.
+# # I am in love with SEVENNTEN.
+# # My bias is The8. 
+# # His real name is Xu minghao.
+# # He is chinese."""
+# # # print(string1)
+
+# # short_quote = """ hello my name is Mia.
+# # I am 1321313 years old.
+# # My dad is sunghoon."""
+# # print(short_quote)
+
+# # long_quote = """ Hello my name is Mia.
+# # My mom is Yuna.
+# # My dad is Sunghoon
+# # My bother are Kia and Soobin.
+# # And I love my family.
+# # I get to meet so many celebertie because of myy parents."""
+# # print(long_quote)
+
+# journal = """ I love to read books.
+# My favorit book is nitro type.
+# mike favorit book is your mom.
+# My moms favorit book is my dad.
+# An my dad is your grandmas.
+# To bad she died.
+# I KILLED HER."""
+# print(journal)
+
+# string1 = " I love coding!"
+# print(string1.lower())
+
+# string2 = " I love Minghao!"
+# print(string1.upper())
+
+# string3 = "coding is cool!"
+# # print(string1.replace("cool", "fun"))
+
+
+# # string = "I love dinosaurs so much!"
+
+# # print(string.replace("I", ""))
+
+
+# # string = "I love otters so much!"
+
+# # print(string.replace("otters", "manatees"))
+
+# string1 = "My name is Simon." 
+# print(string1.lower())
+
+string1 = input("Free food for every 1." )
+print(string1.upper())
