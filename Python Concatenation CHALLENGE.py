e41f4027b578b3c226bf2e8f4def9e6a580ac92f
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-23 12:20:01 -0700

    exported from Skill Struck

---
+++ Python Concatenation CHALLENGE.py
@@ -0,0 +1,69 @@
+                                        # snickers = int(input("How many snickers you got"))
+# nerds = int(input("How many nerds you got"))
+# butterfingers = int(input("How many butterfingers you got"))
+
+# total = snickers + nerds + butterfingers
+
+
+# print("This year, you got " + str(snickers) + " snickers, " + str(nerds) + " nerds, and " + str(butterfingers) + " butterfingers. The total number of these candies is " + str(total) + " candies.")
+
+
+# recipient = int(input("How many snickers you got"))
+# place = int(input("How many nerds you got"))
+# minutes = int(input("How many butterfingers you got"))
+
+# place = int(input("Where do you want to meet?"))
+# minutes = int(input( 
+
+
+# # print("I have an important message for " + recipient + ". Meet me at the " + place + " in " + minutes + " minutes. From, Detective Buggy." )
+
+# recipient = input("Who is the message for?")
+
+# place = input("Where do you want to meet?")
+
+# minutes = input("In how many minutes would you like to meet?")
+
+# # print("I have an important message for " + recipient + ". Meet me at the " + place + " in " + minutes + " minutes. From, Detective Buggy = 3caterpillars = 3
+# # leaves = 25
+
+# # print(caterpillars * leaves)
+
+# rocks = 15
+
+# print(rocks / 3)
+
+# print(5 + 3 * 4 + 3)
+
+# c = 20 * 5
+# print(c)
+
+# apples = 10
+# oranges = 5
+# print(apples + oranges)
+
+# z = 15 / 3
+# print(z)
+
+# a = 7
+# b = 3
+# print(a * b)
+# c =18 / 2
+# # print(c)
+
+
+# SVT = int(input("What is your first number?"))
+
+
+# TXT = int(input("What is her first number?"))
+# print(TXT + SVT)
+
+number_of_people = int(input("how many people will help"))
+
+anount_of_money = int(input("How much wwill each person bring"))
+
+total_money = number_of_people*anount_of_money
+
+print(total_money)
+
+
