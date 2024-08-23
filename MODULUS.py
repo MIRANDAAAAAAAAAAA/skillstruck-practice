e77f0344af82a7295ccbf419380b29db06cd4830
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-23 12:20:02 -0700

    exported from Skill Struck

---
+++ MODULUS.py
@@ -0,0 +1,25 @@
+# number = (int(input(" Enter a number ")))
+# svt = number % 4
+# print(svt)
+
+# # number = (float(input(" Enter a number ")))
+# # svt = number % 2
+# # print(svt)
+
+# family = int(input("Number of family members"))
+# oranges = int(input("Number of oranges"))
+
+# svt = oranges % family
+# print(svt)
+
+
+# Num = int(input("Two diget numbers "))
+# svt = Num / 65
+
+# number = int(input("Pick a number that is two digits "))
+
+# tens = number / 10
+
+# number = int(input("Pick a number that is 10 or greater"))
+# last_two_digits = number % 100
+# print(last_two_digits)
