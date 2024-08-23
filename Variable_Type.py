45972623bba68b05ca8120bed4ea232242b5e9f0
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-23 12:20:00 -0700

    exported from Skill Struck

---
+++ Variable_Type.py
@@ -0,0 +1,145 @@
+# # finished = "true"
+
+# # name = "Hello!"
+# # age = 42
+# # name = True
+
+# # SEVENTEEN = 8
+# # SEVENTEEN2 ="8"
+# # print(SEVENTEEN)
+# # print(SEVENTEEN2)
+
+# # SEVENTEEN = fish
+# # SEVENTEEN2 ="fish"
+# # print(SEVENTEEN)
+# # print(SEVENTEEN2)
+
+# # SEVENTEEN = fishbowl
+# # SEVENTEEN2 ="fishbowl"
+# # print(SEVENTEEN)
+# # print(SEVENTEEN2)
+
+# # name = True
+# # name = False
+
+# # age = 24
+
+# # siblings = 3
+
+# # temperature = -50
+
+# # gpa = 4.0
+
+# # pizza_slices = 2.5
+
+# # dessert_slices = -1.75
+
+
+# hamburger = 4
+# fries = 5.88
+# candy = 8
+# sour_patch = 12.54
+# movie_ticket = 2
+# SVT = 25.86
+# video_game = 12
+# TXT = 32.98
+
+# I enjoy working in teams = 3
+
+# I like to try to things = 3
+
+# I love to be outside = 5
+
+# # Schoool can be way to hard for me = 5
+
+
+# q1 = int(input("I enjoy working in teams."))
+
+# q2 = int(input("I like to try new things."))
+
+# q3 = int(input("I love to be outside."))
+
+# q4 = int(input("I want to travel the world."))
+
+# q5 = int(input("School can be way hard for me."))
+
+
+# q1f =5.50
+
+# q2f =4.50
+
+# q3f =2.28
+
+# qf4 =5.100
+
+# q5f =2.23
+
+# watermelons = 0.5
+
+# converted_float = int(watermelons)
+
+# print(converted_float)
+
+# SVT = "23"
+# watermelons = 0.5
+
+# converted_float = int(watermelons)
+
+# # print(converted_float)
+
+# watermelons = 0.5
+
+# converted_float = int(watermelons)
+
+# print(converted_float)
+
+# family = int(input("How may family members do you have?"))
+# baby = family + 1
+# print(baby)
+
+# txt = float(family)
+# print(txt)
+
+# print("4" * 3)
+
+# age = int(input(""))
+
+# SVT = float(input("Type in a decimal."))
+# integer = int(SVT)
+# print(integer)
+
+# SVT = input("Number")
+# integer = int(SVT)
+# print(integer + 18)
+
+# string1 = "SVT is "
+# string2 = "awesome "
+# string3 = "TXT"
+# string4 = string1 + string2 + string3
+
+# print(string4)  
+
+# string1 = "I like to eat"
+# string2 = "donuts."
+# print(string1 + string2)
+
+# string1 = "I like to"
+# # string2 = "swim."
+
+# # print(string1 + " " + string2)
+
+# string1 = "I won "
+# string2 = "the game."
+
+# print(string1 + string2)
+
+snickers = int(input("How many snickers you got"))
+nerds = int(input("How many nerds you got"))
+butterfingers = int(input("How many butterfingers you got"))
+
+total = snickers + nerds + butterfingers
+
+
+print("This year, you got " + str(snickers) + " snickers, " + str(nerds) + " nerds, and " + str(butterfingers) + " butterfingers. The total number of these candies is " + str(total) + " candies.")
+
+
