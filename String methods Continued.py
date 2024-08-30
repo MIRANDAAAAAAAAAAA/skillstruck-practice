56c7d33c6c902c82b24187b5f47d88bf7d413a07
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-30 12:53:08 -0700

    exported from Skill Struck

---
+++ String methods Continued.py
@@ -0,0 +1,62 @@
+
+# # SLICING METHOD
+
+# string1 = "I love coding!"
+# print(string1[4:])
+
+# # SPLIT METHOD
+
+# string1 = " Ilove spaghettii!"
+
+# print(string1.split())
+
+# LENGTH METHOD
+
+# string1 = "I love coding!"  
+
+# print(len(string1))
+
+# # FIND METHOD
+
+# string1 = " I love Korea!"
+
+# print( string1.find("r"))
+
+
+
+
+
+
+# string = "My favorite animal is the platypus!"
+
+# print(string.rfind("p"))
+
+# string1 = "Popcorn is so good at a movie." 
+# print(string1[4])
+
+# string1 = "Bunjee jumping sounds scary." 
+# print(string1[6])
+
+# string1 = "Snorkeling is fun!" 
+# print(string1[-3])
+
+# string1 = "The quick brown fox jumped over the lazy dog" 
+# print(string1[4:9])
+
+# string1 = "The quick brown fox jumped over the lazy dog" 
+# print(string1[8:])
+
+# string1 = "I saw three ducks this morning"
+
+
+# string1 = "I saw three ducks this morning"
+
+# print(string1.split())
+
+# string1 = "I saw three ducks."
+
+# print(len(string1))
+
+string1 = "Firetrucks are red"
+
+print(string1.find("t"))
