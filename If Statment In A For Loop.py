0b4439b181805259a70ea897bcf28d1b82607370
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-30 12:53:12 -0700

    exported from Skill Struck

---
+++ If Statment In A For Loop.py
@@ -0,0 +1,65 @@
+
+# foods = ["mushrooms", "broccoli", "fish", "olives", "spinach"]
+
+# for food in foods:
+ 
+#     if food == "mushrooms":
+#         print(food + " are gross")  # Concatenated sentence for mushrooms
+   
+#     elif food == "broccoli":
+#         print(food + " is gross")  # Concatenated sentence for broccoli
+  
+#     elif food == "fish":
+#         print(food + " is gross")  # Concatenated sentence for fish
+    
+
+#         print(food + " might not be gross to everyone, but I don't like it")  # Different sentence for other foods
+
+
+
+
+# # Step 1: Create a list of the grossest foods and assign it to a variable named foods
+# foods = ["mushrooms", "broccoli", "fish", "olives", "spinach"]
+
+# # Step 2: Loop through the list named foods using a for loop
+# for food in foods:
+#     # Step 3: Use an if statement to check if the item is equal to "mushrooms"
+#     if food == "mushrooms":
+#         print(food + " are gross")  # Concatenated sentence for mushrooms
+    
+#     # Step 4: Use an elif statement to check if the item is equal to "broccoli"
+#     elif food == "broccoli":
+#         print(food + " is gross")  # Concatenated sentence for broccoli
+    
+#     # Step 5: Use an elif statement to check if the item is equal to "fish"
+#     elif food == "fish":
+#         print(food + " is gross")  # Concatenated sentence for fish
+    
+#     # Step 7: Use an else statement to handle any items not in the above categories
+#     else:
+#         print(food + " might not be gross to everyone, but I don't like it")  # Different sentence for other foods
+
+
+
+# colors = ["red", "blue", "purple", "green", "pink", "yellow", "orange"]
+
+# # print(colors[3:6])
+
+# my_list = [3, 1, 10, 4]
+
+# total = 0
+
+# for x in my_list:
+# 	total = total + x
+	
+# print(total)
+
+butterflies = [14, 17, 12, 14, 15, 18, 19, 10]
+
+for x in butterfly:
+	if x >= 12:
+		print("You have many butterflies.")
+	else:
+		print("You don't have many butterflies.")
+
+        my_list = [3, 1, 10, 4]
