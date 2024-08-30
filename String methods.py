6410fa910303161db8e831d58896c19fd8625b21
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-30 12:53:07 -0700

    exported from Skill Struck

---
+++ String methods.py
@@ -0,0 +1,6 @@
+# svt = input("I just wanted to thank you! I loved how much you helped. I will need to pay it forward!")
+# print(svt.replace("I", "We"))
+
+
+
+CHALLENGE ("Group Thank You Messages")
