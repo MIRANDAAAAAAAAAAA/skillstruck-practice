9a91ecabb98029405a8b9c30e122a4084b163858
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-08-30 12:53:11 -0700

    exported from Skill Struck

---
+++ Lists.py
@@ -0,0 +1,140 @@
+# candy = [ "sweets","sour","tasty","yummy"]
+# print(candy)
+
+# pieces = [ 43,51,88,21]
+# print(pieces)
+
+# pets = ["dog", "cat", 3, 5, "chicken"] 
+# print(pets)
+
+
+# # # friends = ["Mighao","Jun","Joshua"]
+# # # print(friends)
+
+# # ages = [88,8,18]
+# # print(ages)
+
+
+# # # Create two variables with integer values
+# # a = 5
+# # b = 10
+
+# # # Create an if statement with a conditional to compare the two variables
+# # if a > b:
+# #     # Print statement that runs if the conditional is met
+# #     print("Variable 'a' is greater than variable 'b'.")
+# # else:
+# #     # Print statement that runs if the conditional is not met
+# #     print("Variable 'a' is not greater than variable 'b'.")
+
+
+
+# # # Create two variables with integer values
+# # # x = 8
+# # # y = 3
+
+
+# # # if x > y:
+# # #     print("x is greater than y.")
+# # # # else:
+# # # #     print("x is not greater than y.")
+
+
+# # # # fish = 4
+# # # # if fish == 5:
+# # # #     print("We have five fish.")
+# # # # else:
+# # # #     print("We have a different number than five fish.")
+
+# # #     favorite = "blue"
+# # # if favorite == "blue":
+# # # #     print("Blue is your favorite color.")
+# # # # else:
+# # # #     print("Blue is not your favorite color.")
+
+
+# # # favorite = "blue"
+# # # if favorite == "blue":
+# # #     print("Blue is your favorite color.")
+# # # else:
+# # #     print("Blue is not your favorite color.")
+
+# # # Get two numbers as input from the user
+# # num1 = int(input("Enter the first number: "))
+# # num2 = int(input("Enter the second number: "))
+
+# # # Determine and print the smallest of the two numbers
+# # if num1 < num2:
+# #     print(num1)
+# # else:
+# #     print(num2)
+
+
+
+# # Get a number as input from the user
+# number = int(input("Enter a number: "))
+
+# # # Determine if the number is positive or negative
+# # if number >= 0:
+# #     print(1)
+# # else:
+# #     print(-1)
+
+
+
+# age = int(input("Enter your age: "))
+
+# license = True
+
+# if age >= 16 and license == True:
+#     print("You are old enough to drive.")
+# else:
+#     print("You are not able to drive.")
+
+# dogs = 5
+
+# if dogs > 5:
+#     print("You have a lot of dogs")
+# else:
+# #     print("You have a few dogs")
+
+
+# candies = 100
+
+# if candies >= 100:
+#     print("You have enough candy for the party!")
+# else:
+# #     print("You need to get more candy.")
+
+
+#     # Create input that asks the user to enter their height in inches
+# height = int(input("Enter your height in inches: "))
+
+# # Check if the user can ride the roller coaster
+# if height >= 48:
+#     print("You can ride the roller coaster.")
+# # else:
+# #     print("You can't ride the roller coaster.")
+
+
+# # Receive a number as input
+# number = int(input("Enter a number between 0 and 100: "))
+
+# # Check the conditions and print the appropriate output
+# if number <= 50:
+#     print("Fewer than average")
+# else:
+#     print("More than average")
+
+
+# Create the variable
+coins = 10
+
+# Compare the variable and print the appropriate response
+if coins > 20:
+    print("You have more than enough to buy a puppy.")
+elif coins == 20:
+    print("You have exactly enough to buy a puppy.")
+else:
+    print("You do not have enough to buy a puppy.")
+
