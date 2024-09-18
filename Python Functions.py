db385b7118dac6ab7687bd77bbc6667ed33f95a8
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-09-18 12:06:51 -0700

    exported from Skill Struck

---
+++ Python Functions.py
@@ -0,0 +1,42 @@
+# PYTHON FUNCTION
+# ->
+# def race():
+#     print("Xu won the race!")
+
+# race()
+
+# CHARLIE THE DOG
+# ->
+# def charlie():
+#     print("Charlie is great!")
+
+
+# charlie()
+
+# START THE GAME
+# ->
+start = input("Would you like to launch the game? enter yes or no")
+
+def start_game():
+    if start == "yes":
+        print("Initialization Complete")
+    else:
+        print("Initialization Failed")
+
+start_game()
+
+
+
+
+
+
+
+
+# my_number = int(input("Pick a number."))
+# total = my_number
+# while my_number != 0:
+#     my_number = int(input())
+#     total = total + my_number
+    
+# print(total)
+
