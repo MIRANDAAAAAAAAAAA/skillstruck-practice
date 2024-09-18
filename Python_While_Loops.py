17abf14940568b402f1381d666777c38ffce3586
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-09-18 12:06:50 -0700

    exported from Skill Struck

---
+++ Python_While_Loops.py
@@ -0,0 +1,65 @@
+# IDK
+# ->
+# people_in_line = 20
+# while people_in_line > 0:
+#     print("There are " + str(people_in_line) + " people in line.  Keep the coaster running!")
+#     people_in_line = people_in_line - 1
+
+# * DECREMNT OPERATOR *
+#     people_in_line = people_in_line - 1
+
+#     - Every time the loop runs, the variable named people_in_line is subtracted by one.
+
+# INCREMENT OPERATOR
+# ->  An increment operator works the same way as a decrement operator.  The increment simply adds to the variable instead of subtracting.
+
+# people_in_line = people_in_line + 1
+
+# people_in_line += 1
+
+# WHILE LOOP EXSAMPLE
+# ->  * Let's say you wanted to write a program where the user can input their age and the loop will print out all the numbers below their age. We can use a while loop to do this!
+# age = 17
+
+# while age >+ 0: 
+#     print(age)
+#     age -= 1
+
+# WHILE LOOPS
+# ->
+# number = 1
+# while number <= 0:
+#     if number == 1 or number == 50:
+#         print(number)
+#         number +=1
+
+# HOW MANY numbers
+# ->
+# total = 0
+# number = int(input())
+# while number != 0:
+#     total += 1 
+#     number = int(input())  
+# print(total)
+
+# SUM OF NUMBERS
+# ->
+
+
+my_number = int(input("Pick a number."))
+total = my_number
+while my_number != 0:
+    my_number = int(input())
+    total = total + my_number
+    
+print(total)
+
+
+
+
+
+
+
+
+
+
