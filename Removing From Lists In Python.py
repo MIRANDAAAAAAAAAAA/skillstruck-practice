561f9d7d6af7dbb4b6e92cc5da60c612a3bded2b
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-09-18 12:06:49 -0700

    exported from Skill Struck

---
+++ Removing From Lists In Python.py
@@ -0,0 +1,86 @@
+# frogs = ["scoups", "jun", "vernom", "the8", "Xuminghao"]
+
+# frogs.remove("vernom")
+
+# print(frogs)
+# ^
+# REMOVE
+
+# jun = ["xu" ,"josh", "gabe", "jake"]
+
+# the8 = jun.pop(3)
+
+# print(the8 + " is my favorite member")
+
+# print(jun)
+# ^
+# POP
+
+
+
+
+# harvest = ["pumpkins", "apples", "corn", "squash", "carrots"]
+
+# harvest.remove("squash")
+
+# print(harvest)
+# ^
+# CODE CHECKPOINT
+
+# harvest = ["pumpkins", "apples", "corn", "squash", "carrots"]
+
+# fall = harvest.pop(1)
+
+# print(fall + " is my favorite fruit")
+
+# print(harvest)
+# ^
+# CODE CHECKPOINT
+
+
+
+#  choice = input("What skill do you want to remove?")
+
+# skills = ["Piano", "Soccer", "Coding", "Cooking", "Writing"]
+
+# skills.remove(choice)
+
+# print(skills)
+# ^
+# SKILL (CHALLENGE)
+
+
+
+# gold = input("Miguel Elizabeth Gabe Betty Alaya").split()
+
+# print(gold.pop(2) + " got the bronze medal")
+# ^
+# BRONZE MEDAL (CHALLENGE)
+
+
+
+# insects = ["dragonfly", "bee", "butterfly", "katydid", "lacewing"]
+
+# insects.remove("bee")
+
+# print(insects)
+
+
+# insects = ["dragonfly", "bee", "butterfly", "katydid", "lacewing"]
+
+# favorite = insects.pop("katydid")
+
+# print(favorite)
+
+
+# insects = ["dragonfly", "bee", "butterfly", "katydid", "lacewing"]
+
+# prize = insects.pop(1)
+
+# print(insects)
+
+insects = ["dragonfly", "bee", "butterfly", "katydid", "lacewing"]
+
+prize = insects.pop(1)
+
+print(prize)
