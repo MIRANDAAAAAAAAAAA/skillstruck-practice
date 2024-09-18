e8facdefc8035e2c9854710168f45996dd359210
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-09-18 12:06:52 -0700

    exported from Skill Struck

---
+++ Pythino_Function_Parameters.py
@@ -0,0 +1,92 @@
+# BASIC PYTHON
+# ->
+# def the8():
+#     print("the concert was so good")
+
+
+# the8()    
+
+# PARAMETERS
+# ->
+# def weather(type):
+#     print(" It's a soggy day outside because it is " + type)
+
+
+
+# weather()
+
+# CHECK POINT
+# ->
+# # def minghao(type): 
+# #     print (" Hyunjin is not hot at all" + type)
+
+
+
+# # minghao("minghao")
+# # minghao("your_mom")
+# # minghao("shut_up_motiaa")
+
+
+
+# fruit = input("Please enter the name of a fruit: ")
+
+# def hungryForApples(choice):
+#     print( fruit+fruit+fruit )
+	
+# hungryForApples(fruit)
+
+
+
+
+# month = int(input("Which month?  1-12"))
+
+# def invitation (choice) :
+# 	if choice == 1 or choice == 3 or choice == 5 or choice == 7 or choice == 8 or choice == 10 or choice == 12:
+# 		print("31")
+# 	elif choice == 4 or choice == 6 or choice == 9 or choice == 11:
+# 		print("30")
+# 	else:
+# 		print("28") 
+	
+
+# invitation(month)
+
+
+
+
+
+# def name (first, last) :
+# 	print("The first name is " + first )
+# 	print("The last name is " + last )
+	
+
+# name("jake", "hoshi")
+# name("minghao", "jun")
+
+
+
+
+# 
+
+first = int(input("Choose a number"))
+second = int(input("Choose a number"))
+third = int(input("Choose a number"))
+fourth = int(input("Choose a number"))
+fifth = int(input("Choose a number"))
+
+
+def my_function(choice1, choice2, choice3, choice4, choice5):
+	smallest = choice1
+	if choice2 < choice1:
+		smallest = 
+	if choice3 < choice2:
+		smallest = 
+	if choice4 < choice3:
+		smallest = 
+	if choice5 < choice4:
+		smallest = 
+	print(my_function)
+	
+		
+	
+(choice1,choice2 choice3,choice4,choice5)
