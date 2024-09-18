7f6ebe202e0a441ebaf186348ed13c6d39fc6dc9
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-09-18 12:06:44 -0700

    exported from Skill Struck

--- CHALLENGES ( SMC).py
+++ CHALLENGES ( SMC).py
@@ -1,23 +1,13 @@
-# my_list = [int(n) for n in input().split()]
-# total = 0
-
-# for x in my_list:
-#     total = total + x
-# #print(len(my_list))	
-# average = total/len(my_list)
-# #print(average)
-# for x in my_list:
-#     if x > average:
-#         print(x)
-
-letter = input("Choose a letter")
+choice = input("Choose a letter")
 
 my_list = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white", "gray", "pink", "indigo", "brown", "tan", "gold", "silver"]
 
 total = 0
 
 for x in my_list:
-	if letter in x: 
-		total = total + x
-		
-print(total)
+if choice in x:
+total = total + 1
+
+print("There are " + str(total) + " items in the list that have the letter " + choice + " in it.")
+
+print(helow world)
