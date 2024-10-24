b1c556f3d65e69e3583af93a9b7b16fb8bfd5b7b
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-10-24 12:26:05 -0700

    exported from Skill Struck

---
+++ Python_Dictionaries.py
@@ -0,0 +1,112 @@
+# #  DICTIONARIES
+#  ->
+# cute = { 
+#     "joshua" : 8,
+#     "hoshi" : 12,
+#     "minghao" : 13,
+# }  
+
+# print(cute)
+# #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # ANOHER EXAMPLE
+# # *   you can put names as well
+# ->
+# cute = {
+#     "string1" : 12,
+#     "string2" : 33,
+#     "string3" : 56,
+#     "string4" : 35,
+# }
+
+# print(cute)
+# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # Accessing Items in the Dictionary
+# ->
+# cute = {
+#     "minghao" : 8,
+#     "scoups" : 1,
+#     "hoshi" : 2,
+#     "mingyu" : 3,
+#     "vernom" : 4,
+#     "jun" : 5,
+#     "wonwoo" : 6,
+#     "sunkuan" : 7,
+#     "joshua" : 9,
+#     "woozi" : 10,
+#     "dk" : 11,
+#     "dino" : 12,
+#     "jeonghan" : 13
+# }
+
+# print(cute["minghao"])
+# #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# # Changing Values in the Dictionary
+# ->
+# cute = { 
+#     "minghao" : 8,
+#     "scoups" : 1,
+#     "hoshi" : 2,
+#     "mingyu" : 3,
+#     "vernom" : 4,
+#     "jun" : 5,
+#     "wonwoo" : 6,
+#     "sunkuan" : 7,
+#     "joshua" : 9,
+#     "woozi" : 10,
+#     "dk" : 11,
+#     "dino" : 12,
+#     "jeonghan" : 13
+# }
+
+# cute["minghao"] = 13
+
+# print(cute)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# checkpoint
+# ->
+# mountains = {"Timpanogos" : 5, "Everest": 10, "Kilimanjaro": 6, "Vesuvius": 7}
+
+# print(mountains)
+
+# print(mountains["Vesuvius"])
+
+# mountains["Kilimanjaro"] = 5
+# print(mountains)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CHALLENGE (DICTINOARY AUTHOR
+# ->
+# cute = { 
+#     "joshua" : 8,
+#     "hoshi" : 12,
+#     "minghao" : 13,
+#     "scoups" : 1,
+#     "hoshi" : 2,
+#     "mingyu" : 3,
+#     "vernom" : 4,
+#     "jun" : 5,
+#     "wonwoo" : 6,
+  
+# }  
+
+# print(cute)
+#  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CHALLENGE(DICTONARIES AND LISTS)
+# ->
+# cute = { 
+#     "joshua" : 8,
+#     "hoshi" : 12,
+#     "minghao" : 13,
+#     "scoups" : 1,
+#     "hoshi" : 2,
+    
+  
+# }  
+
+# print(cute)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# The seconde part of he challenge is this one
+
+cute_list = ["shoes", "socks", "shirts", "pants", "pjs"]
+
+print(cute_list)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
