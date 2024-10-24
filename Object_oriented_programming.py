21ee7a96007a561cb5a97ebf76854e46c985deca
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-10-24 12:26:25 -0700

    exported from Skill Struck

---
+++ Object_oriented_programming.py
@@ -0,0 +1,25 @@
+# class Person:
+#     def __init__(self, name, age, gender):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+
+
+class Tree:
+    def __init__(self, fruit, height, leaf, flower, seed):
+        self.fruit = fruit
+        self.height = height
+        self.leaf = leaf
+        self.flower = flower
+        self.seed = seed
+
+
+
+
+# class Tree:
+#     def __init__(self, fruit, height, leaf, flower, seed):
+#         self.fruit = fruit
+#         self.height = height
+#         self.leaf = leaf
+#         self.flower = flower
+#         self.seed = seed
