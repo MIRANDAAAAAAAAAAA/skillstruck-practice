cc6bab27d8c4cdfd513d712a026301307ee794e4
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-10-24 12:26:17 -0700

    exported from Skill Struck

---
+++ Reading parts of a file.py
@@ -0,0 +1,15 @@
+# number = int(input(" What number do you want to print?"))
+# file = open("hi.txt", "r")
+# print(file.read(number))
+# file.close()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+file = open("sike.txt", "r")
+print(file.readlines())
+print(file.readlines())
+file.close
+
+
+
+
+
+
