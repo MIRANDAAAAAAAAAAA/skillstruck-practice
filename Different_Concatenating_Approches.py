54311329d82f3c1f759b165473b1aa996cfbd26f
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-11-20 12:38:41 -0700

    exported from Skill Struck

---
+++ Different_Concatenating_Approches.py
@@ -0,0 +1,77 @@
+# Plus Symbol + Approach:
+# name = "Jun"
+# age = 28
+# grade = 10
+
+# print("Hello my name is " + name + " and I am " + str(age) + " years old and am in grade " + str(grade))
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Comma Approach:
+# member = "Xuminghao"
+# unit = 8
+
+# print("Today we have", member, "photocards in stock and", unit)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CHECKPOINT:
+# name = "Jun"
+# dogs = 28
+# location = "10"
+
+# print("My name is " + name + " and I have " + str(dogs) + " dogs and live in " + location)
+
+# a = 32
+# b = 79
+# c = 3
+# d = 5
+# e = 28
+
+# print( 32 , 79 , 3 , 5 , 28 )
+# print("My name is", name,"and I have", dogs, "dogs and live in", location)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CHALLENGES:1
+# * Create 5 variables and set them equal to an integer
+
+# * Use the comma method to concatenate together your 5 variables.
+
+# a = 32
+# b = 79
+# c = 3
+# d = 5
+# e = 28
+
+# print( 32 , 79 , 3 , 5 , 28 )
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CHALLENGES:2
+
+# * Create 5 variables and set them equal to strings.
+
+# * Create 5 variables and set them equal to integers
+
+# * Use the f string method to concatenate all the variables together into one statement.
+
+# a = 32
+# b = 79
+# c = 3
+# d = 5
+# e = 28
+# name = "32, 79, 3, 5, 28"
+# print( 32 , 79 , 3 , 5 , 28 )
+
+
+# print(f"My name is {name} and I have {dogs} dogs and live in {location}")
+
+
+# fruits = "hi"
+# veggies = "hello"
+# carrot = "crazy"
+# berry = "cool"
+# mango = "smart"
+
+# a = 32
+# b = 79
+# c = 3
+# d = 5
+# e = 28
+
+# print(f"{fruits}, {veggies}, {carrot}, {berry}, {mango}, {a}, {b}, {c}, {d}, {e}")
+
+
