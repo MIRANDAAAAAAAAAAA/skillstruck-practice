535335a945c92f582817632f90b880e5b5c95c6a
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-11-20 12:38:40 -0700

    exported from Skill Struck

---
+++ Printing_Specifics_in_Python.py
@@ -0,0 +1,27 @@
+print("I love code!")
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Print on a New Line:
+
+print("I love \n code!")
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print("Minghao \n The8 \n Frog")
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Add a Tab:
+
+print("I love \t to swim")
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Carriage Return:
+A carriage return prints everything after the symbol.
+
+print("Today is \r a new day to see the sun!")
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+CHALLENGE:1
+4 print statements total that say the class names. 3 should add a tab in the print statement.
+
+print("Senior \n   junior \n      Sophomore  \n           Freshman")
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+CHALLENGE:2
+Print the phrase "Once upon a time" with each word appearing on a new line. Use only one print statement.
+
+print("Once \n  upon \n   a  \n    time")
