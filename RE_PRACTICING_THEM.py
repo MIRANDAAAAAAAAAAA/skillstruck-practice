e98e935f8172b87c585434f7c479f232fbd930e8
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-11-20 12:38:38 -0700

    exported from Skill Struck

---
+++ RE_PRACTICING_THEM.py
@@ -0,0 +1,31 @@
+# print("Hello World")
+
+# age = 15
+
+# pet = "dog"
+
+# age = 27
+
+# favorite = input("Your favorite color is ?")
+
+# print(" My fvorite color is " + favorite)
+
+
+
+# color = input("What is your favorite color?")
+
+# print("Your favorite color is " + color)
+# # This is a comment
+# print("Did you see my comment?")
+
+
+
+# This is a comment
+print("Did you see my comment?")
+
+# this is another comment
+
+# This is minghao
+print("Did you see my minghao?")
+
+# this is another minghao
