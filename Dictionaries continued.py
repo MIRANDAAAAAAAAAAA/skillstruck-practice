37a6d173a820eef6ec2cab0f111f6f40007024d3
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-12-11 13:06:29 -0700

    exported from Skill Struck

--- Dictionaries continued.py
+++ Dictionaries continued.py
@@ -1,100 +1,100 @@
-# Dictionary Length
-# # ->
-# cute = {
-#     "minghao": 13,
-#     "hoshi": 3,
-#     "woonwoo": 56,  
-#     "mingyu": 34,
-#     "woozi": 25,
-#     "vernom": 987,
-# }
-
-# print(len(cute))
-
-# &   You can check to see how many items are in the dictionary.  As with Python lists, when counting length we start counting at 1.
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Check if Key is in the Dictionary
+Dictionary Length
 # ->
-# the_classmates = {
-#       "minghao": 13,
-#     "hoshi": 3,
-#     "woonwoo": 56,  
-#     "mingyu": 34,
-#     "woozi": 25,
-#     "vernom": 987,
-# }
+cute = {
+    "minghao": 13,
+    "hoshi": 3,
+    "woonwoo": 56,  
+    "mingyu": 34,
+    "woozi": 25,
+    "vernom": 987,
+}
 
-# if "vernom" in the_classmates:
-#     print("vernom is in your dictionary")
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- # *You can also use not in to check your dictionary.
-#     ^
-# classmates = {
-#       "minghao": 13,
-#     "hoshi": 3,
-#     "woonwoo": 56,  
-#     "mingyu": 34,
-#     "woozi": 25,
-#     "vernom": 987,
-# }
+print(len(cute))
 
-# if "vernom" in classmates:
-#     print("vernom is not in your dictionary")
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Converting from a Python list to a dictionary
-# ->
-# group = ["minghao", "scoups", "hoshi", "mingyu", "vernom"]
+&   You can check to see how many items are in the dictionary.  As with Python lists, when counting length we start counting at 1.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Check if Key is in the Dictionary
+->
+the_classmates = {
+      "minghao": 13,
+    "hoshi": 3,
+    "woonwoo": 56,  
+    "mingyu": 34,
+    "woozi": 25,
+    "vernom": 987,
+}
 
-# group_dictionary = dict.fromkeys(group, "seventeen")
+if "vernom" in the_classmates:
+    print("vernom is in your dictionary")
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *You can also use not in to check your dictionary.
+    ^
+classmates = {
+      "minghao": 13,
+    "hoshi": 3,
+    "woonwoo": 56,  
+    "mingyu": 34,
+    "woozi": 25,
+    "vernom": 987,
+}
 
-# print(group_dictionary)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# CODE CHECKPOINT
-# ->
-# coins = {
-#    		 "pennies" : 1,
-#    		 "nickels": 5,
-#   		 "dimes": 10,
-#   		 "quarters": 25,
-# }
+if "vernom" in classmates:
+    print("vernom is not in your dictionary")
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Converting from a Python list to a dictionary
+->
+group = ["minghao", "scoups", "hoshi", "mingyu", "vernom"]
 
-# print(coins)
-# coins["silver dollars"] = 10 ;
-# coins.pop("pennies")
-# print(coins)
-# print(len(coins))
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+group_dictionary = dict.fromkeys(group, "seventeen")
+
+print(group_dictionary)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+CODE CHECKPOINT
+->
+coins = {
+   		 "pennies" : 1,
+   		 "nickels": 5,
+  		 "dimes": 10,
+  		 "quarters": 25,
+}
+
+print(coins)
+coins["silver dollars"] = 10 ;
+coins.pop("pennies")
+print(coins)
+print(len(coins))
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-# dictionary = {
-# 	1: "bicycle",
-# 	2: "soccer balls",
-# 	3: "piano books"
-# }
+dictionary = {
+	1: "bicycle",
+	2: "soccer balls",
+	3: "piano books"
+}
 
-# dictionary[4] = input("What do you have 4 of?")
-# dictionary[5] = input("What do you have 5 of?")
-# dictionary[6] = input("What do you have 6 of?")
+dictionary[4] = input("What do you have 4 of?")
+dictionary[5] = input("What do you have 5 of?")
+dictionary[6] = input("What do you have 6 of?")
 						  
-# print(dictionary)
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print(dictionary)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-# work = {
+work = {
 
-# 	"Monday": 3,
-# 	"Tuesday": 4, 
-# 	"Wednesday": 2,
-# 	"Thursday": 1, 
-# 	"Friday": 1, 
+	"Monday": 3,
+	"Tuesday": 4, 
+	"Wednesday": 2,
+	"Thursday": 1, 
+	"Friday": 1, 
 	
-# }
+}
 
-# work["Saturday"] = 8
-# work.pop("Wednesday")
-# print(len(work))
+work["Saturday"] = 8
+work.pop("Wednesday")
+print(len(work))
 
-# if "Friday" in work:
-# 	print("Friday is in work")
+if "Friday" in work:
+	print("Friday is in work")
 
-# print(work)
+print(work)
