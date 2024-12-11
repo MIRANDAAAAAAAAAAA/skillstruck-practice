3a43579a12fcb667337bfdf3ac7fa56c92017f3e
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-12-11 13:06:22 -0700

    exported from Skill Struck

--- String methods Continued.py
+++ String methods Continued.py
@@ -1,65 +1,65 @@
 
-# # SLICING METHOD
+# SLICING METHOD
 
-# string1 = "I love coding!"
-# print(string1[4:])
+string1 = "I love coding!"
+print(string1[4:])
 
-# # SPLIT METHOD
+# SPLIT METHOD
 
-# string1 = " Ilove spaghettii!"
-
-# print(string1.split())
+string1 = " Ilove spaghettii! jkjkd aksdhka"
+my_list = string1.split("j")
+print(my_list)
 
 # LENGTH METHOD
 
-# string1 = "I love coding!"  
+string1 = "I love coding!"  
 
-# print(len(string1))
+print(len(string1))
 
-# # FIND METHOD
+# FIND METHOD
 
-# string1 = " I love Korea!"
+string1 = " I love Korea!"
 
-# print( string1.find("r"))
-
-
+print( string1.find("r"))
 
 
 
 
-# string = "My favorite animal is the platypus!"
-
-# print(string.rfind("p"))
-
-# string1 = "Popcorn is so good at a movie." 
-# print(string1[4])
-
-# string1 = "Bunjee jumping sounds scary." 
-# print(string1[6])
-
-# string1 = "Snorkeling is fun!" 
-# print(string1[-3])
-
-# string1 = "The quick brown fox jumped over the lazy dog" 
-# print(string1[4:9])
-
-# string1 = "The quick brown fox jumped over the lazy dog" 
-# print(string1[8:])
-
-# string1 = "I saw three ducks this morning"
 
 
-# string1 = "I saw three ducks this morning"
+string = "My favorite animal is the platypus!"
 
-# print(string1.split())
+print(string.rfind("p"))
 
-# string1 = "I saw three ducks."
+string1 = "Popcorn is so good at a movie." 
+print(string1[4])
 
-# print(len(string1))
+string1 = "Bunjee jumping sounds scary." 
+print(string1[6])
 
-# string1 = "Firetrucks are red"
+string1 = "Snorkeling is fun!" 
+print(string1[-3])
 
-# print(string1.find("t"))
+string1 = "The quick brown fox jumped over the lazy dog" 
+print(string1[4:9])
+
+string1 = "The quick brown fox jumped over the lazy dog" 
+print(string1[8:])
+
+string1 = "I saw three ducks this morning"
+
+
+string1 = "I saw three ducks this morning"
+
+print(string1.split())
+
+string1 = "I saw three ducks."
+
+print(len(string1))
+
+string1 = "Firetrucks are red"
+
+print(string1.find("t"))
 
 
 coffe = "I love coding! "
