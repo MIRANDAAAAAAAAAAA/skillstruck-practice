8c1c50dccaa595aedbeb1653b96e376bc9837b7d
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-12-11 13:06:33 -0700

    exported from Skill Struck

--- Append_a_line_to_a_text_file.py
+++ Append_a_line_to_a_text_file.py
@@ -1,37 +1,37 @@
-# file = open("example.txt", "a")
-# file.write("This is the new line we added!")
+file = open("example.txt", "a")
+file.write("This is the new line we added!")
 
 
-# file.close()
-# file = open("example.txt", "r")
-# print(file.read())
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# file = open("example.txt", "a")
-# file.write("I love programing!")
-# file.close()
+file.close()
+file = open("example.txt", "r")
+print(file.read())
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+file = open("example.txt", "a")
+file.write("I love programing!")
+file.close()
 
-# file = open("example.txt", "r")
-# print(file.read())
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+file = open("example.txt", "r")
+print(file.read())
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# # Define the file name
-# file_name = "programming_love.txt"
-
-
+# Define the file name
+file_name = "programming_love.txt"
 
 
-# with open(file_name, "a") as file:
-#     file.write("I love programming!\n")
 
 
-# with open(file_name, "r") as file:
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# file = open("letter.txt", "a")
-# file.write("From your Pen Pal")
-# file.close()
+with open(file_name, "a") as file:
+    file.write("I love programming!\n")
 
-# file = open("letter.txt", "r")
-# print(file.read())
+
+with open(file_name, "r") as file:
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+file = open("letter.txt", "a")
+file.write("From your Pen Pal")
+file.close()
+
+file = open("letter.txt", "r")
+print(file.read())
 
 file = open("report.txt", "a")
 file.write("Quote was said by Gandhi")
