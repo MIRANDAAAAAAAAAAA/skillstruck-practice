fda4447dea03416115428f3ccef1d82978ffccb2
MIRANDAAAAAAAAAAA <051408torres@gmail.com>
2024-12-11 13:06:24 -0700

    exported from Skill Struck

--- Concatenating Numbers.py
+++ Concatenating Numbers.py
@@ -1,86 +1,86 @@
-# # Define integer variables
-# # num_apples = 5
-# # num_oranges = 8
+Define integer variables
+num_apples = 5
+num_oranges = 8
 
-# # # # Define a string variable with two sets of curly brackets
-# # # fruit_string = "I have {} apples and {} oranges."
+# # Define a string variable with two sets of curly brackets
+# fruit_string = "I have {} apples and {} oranges."
 
-# # # # Format and print the string with integer variables
-# # # print(fruit_string.format(num_apples, num_oranges))
+# # Format and print the string with integer variables
+# print(fruit_string.format(num_apples, num_oranges))
 
-# # # string1.format(trees, bushes)
+# string1.format(trees, bushes)
 
-# # # Take input from the user as a string
-# # input_number = input("Input: ")
+# Take input from the user as a string
+input_number = input("Input: ")
 
-# # # Convert each character to an integer and calculate the sum of the digits
-# # sum_of_digits = int(input_number[0]) + int(input_number[1]) + int(input_number[2])
+# Convert each character to an integer and calculate the sum of the digits
+sum_of_digits = int(input_number[0]) + int(input_number[1]) + int(input_number[2])
 
-# # # Print the result using the required format
-# # print("The sum of those digits is {}.".format(sum_of_digits))
-
-
-# # # Take input from the user as a string
-# # input_number = input("Input: ")
-
-# # # Convert each character in the string to an integer and calculate the sum of the digits
-# # sum_of_digits = int(input_number[0]) + int(input_number[1]) + int(input_number[2])
-
-# # # Print the result using the required format
-# # print("The sum of those digits is {}.".format(sum_of_digits))
+# Print the result using the required format
+print("The sum of those digits is {}.".format(sum_of_digits))
 
 
+# Take input from the user as a string
+input_number = input("Input: ")
+
+# Convert each character in the string to an integer and calculate the sum of the digits
+sum_of_digits = int(input_number[0]) + int(input_number[1]) + int(input_number[2])
+
+# Print the result using the required format
+print("The sum of those digits is {}.".format(sum_of_digits))
 
 
-# # Take input from the user as an integer
-# day_of_year = int(input("Input: "))
-
-# # January 1 is a Thursday, which corresponds to day 4
-# starting_day = 4
-
-# # Calculate the day of the week
-# day_of_week = (starting_day + day_of_year - 1) % 7
-
-# # Print the result using the required format
-# print("The day of the week is the number {}.".format(day_of_week))
 
 
-# # Define integer variables
-# num_apples = 5
-# num_oranges = 8
+# Take input from the user as an integer
+day_of_year = int(input("Input: "))
 
-# # # Define a string variable with two sets of curly brackets
-# # fruit_string = "I have {} apples and {} oranges."
+# January 1 is a Thursday, which corresponds to day 4
+starting_day = 4
 
-# # # Format and print the string with integer variables
-# # print(fruit_string.format(num_apples, num_oranges))
+# Calculate the day of the week
+day_of_week = (starting_day + day_of_year - 1) % 7
 
-# # string1.format(trees, bushes)
-
-# # Take input from the user as a string
-# input_number = input("Input: ")
-
-# # Convert each character to an integer and calculate the sum of the digits
-# sum_of_digits = int(input_number[0]) + int(input_number[1]) + int(input_number[2])
-
-# # Print the result using the required format
-# print("The sum of those digits is {}.".format(sum_of_digits))
+# Print the result using the required format
+print("The day of the week is the number {}.".format(day_of_week))
 
 
-# # Take input from the user as a string
-# input_number = input("Input: ")
+# Define integer variables
+num_apples = 5
+num_oranges = 8
 
-# # Convert each character in the string to an integer and calculate the sum of the digits
-# sum_of_digits = int(input_number[0]) + int(input_number[1]) + int(input_number[2])
+# # Define a string variable with two sets of curly brackets
+# fruit_string = "I have {} apples and {} oranges."
 
-# # Print the result using the required format
-# print("The sum of those digits is {}.".format(sum_of_digits)
+# # Format and print the string with integer variables
+# print(fruit_string.format(num_apples, num_oranges))
+
+# string1.format(trees, bushes)
+
+# Take input from the user as a string
+input_number = input("Input: ")
+
+# Convert each character to an integer and calculate the sum of the digits
+sum_of_digits = int(input_number[0]) + int(input_number[1]) + int(input_number[2])
+
+# Print the result using the required format
+print("The sum of those digits is {}.".format(sum_of_digits))
 
 
-# num1 = int(input("number of miles your car can drive in a day"))
-# num2 = int(input("total number of miles to get to your destination"))
-# total = num2/num1
-# print("The total number of days you will need to drive there is " + str(total))
+# Take input from the user as a string
+input_number = input("Input: ")
+
+# Convert each character in the string to an integer and calculate the sum of the digits
+sum_of_digits = int(input_number[0]) + int(input_number[1]) + int(input_number[2])
+
+# Print the result using the required format
+print("The sum of those digits is {}.".format(sum_of_digits)
+
+
+num1 = int(input("number of miles your car can drive in a day"))
+num2 = int(input("total number of miles to get to your destination"))
+total = num2/num1
+print("The total number of days you will need to drive there is " + str(total))
 
 
 # Receive input from the user
